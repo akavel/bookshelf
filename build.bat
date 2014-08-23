9cfebf87b0663ccc43df0955e883eab0da8c1739
Mateusz CzapliĹ„ski <czapkofan@gmail.com>
2014-08-23 20:31:56 +0200

    scripts aiding in compilation on Windows

---
+++ build.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+set ANDROID_HOME=C:\dnload\komrka~1\adt-bundle-windows-x86_64-20130729\sdk
+set PATH=%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%PATH%
+:: android sdk v. 2.1
+set ANDROID_PLATFORM=android-7
+
+::set JAVA_HOME=c:\dnload\java-etc\openjdk_1.6.0_b28_64
+::set JAVA_HOME=c:\Program Files\Java\jdk1.7.0_25
+set JAVA_HOME=c:\Program Files\Java\jdk1.6.0_38
+
+::cmd /k
+
+call ant.bat clean
+call ant.bat debug
+
+endlocal
---
+++ copy-kawa.bat
@@ -0,0 +1,1 @@
+@copy  /y \dnload\java-etc\kawa\kawa-1.14.1.jar libs 
